cb43ec04da90e4410cf7f0c0757c034920240c4b
SKITTLE WAN (INFRA-ISD-OOCLL/HKG) <WANSK@OOCL.COM>
2018-12-06 14:19:06 +0800

    About and User page are added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route,Link} from "react-router-dom"
+import {BrowserRouter as Router, Route,Link,Switch} from "react-router-dom"
+import AboutPage from "./component/AboutPage"
+import NotFoundPage from "./component/NotFoundPage"
+import UserPage from "./component/UserPage"
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="">
         <Router>
           <div>
             <ul>
               <li>
                 <Link to ="/about">about</Link>
               </li>
+              <li>  
+                <Link to ="/user">user</Link>
+              </li>
             </ul>
-            <Route path="/about" component={()=><h2>about</h2>}></Route>
+            <Switch>
+            <Route path="/" exact component={() => <h1>Home</h1>}></Route>
+            <Route path="/about" component={AboutPage}></Route>
+            <Route path="/user" component={UserPage}></Route>
+            <Route component={NotFoundPage}/>
+            </Switch>
           </div>
         </Router>
       </div>
---
+++ src/component/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NotFoundPage extends Component {
+  render() {
+    return (
+      <div>
+        <h2>NotFoundPage</h2>
+      </div>
+    )
+  }
+}
